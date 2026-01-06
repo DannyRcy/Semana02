9f5167505982862ee89c0df43755c9c2dc3f220f
Daniel <71902519@continental.edu.pe>
2026-01-06 18:46:31 -0500

    Suma de dos numeros

--- operaciones.py
+++ operaciones.py
@@ -1,1 +1,10 @@
-print("Hello world")
+while True:
+    try:
+        numero1 = int(input("Ingrese el primer número (entero): "))
+        numero2 = int(input("Ingrese el segundo número (entero): "))
+        break
+    except ValueError:
+        print("Por favor, ingrese un número válido.")
+
+suma = numero1 + numero2
+print("La suma es:", suma)
